dfdf94f061f2ed89a589b63118c9223e4069317c
Jae-Joon Lee <lee.j.joon@gmail.com>
2011-09-09 14:35:21 +0900

    add module documentation

--- mpl_toolkits/annotation_set.py
+++ mpl_toolkits/annotation_set.py
@@ -1,3 +1,33 @@
+"""
+This module provides a way annotate multiple points. Annotating texts
+are drawn along a single (vertical or horizontal) line. And the offset
+between texts are auto adjusted so that they do not overwrap.
+
+Running this command is similar to *annotate*. For example,
+
+    s_list = ["label1", "label2", "label3"]
+    xy_list = [(0, 0), (1, 2), (2, 1)]
+    direction = "top"
+
+    att = axes_annotation_set(ax, direction,
+                              s_list, xy_list,
+                              xycoords='data',
+                              refpos=1.3,
+                              refcoord="data"
+                              )
+
+Here, s_list is a list of labels and the xy_list is a list of xy
+positions to be annotated. *xycoords* has same meaning as in the
+*annotate* and it aplies to position in *xy_list*. The *direction*
+argument specifies the direction of annotated texts. *refpos* and
+*refcoord* is position and coordinate for annotating texts. Unlike
+*annotate*, you must specify a single value for the position. For
+direction of "top" and "bottom", the *refpos* means y-position of
+annoatating texts and x-positions are automatically determined. For
+direction of "left" and "right", the *refpos* means x-position of
+texts and y-positions are automatically adjusted.
+"""
+
 import matplotlib.transforms as mtransforms
 from matplotlib.text import Annotation
 
